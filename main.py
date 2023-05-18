3472a14ed76b5f5885ca2f277e04dccf316c880b
quangnhnhut123 <quangnhnhut@gmail.com>
2023-05-18 14:43:56 +0700

    Using docker image instead of build Dockerfile

--- main.py
+++ main.py
@@ -61,42 +61,42 @@ def files():
         for file in files:
             # Getting the file name and content
             file_name = file.filename
-
-            if re.search(r"\.(md|DS_Store|png|gitignore)$", file_name):
-                continue
-            elif file_name in processed_files:
+            if file_name in processed_files:
                 # Skip processing duplicate file names
                 continue
             else:
-                try:
-                    content = repo.get_contents(
-                        file_name, ref=commit.sha
-                    ).decoded_content
+                if re.search(r"\.(md|DS_Store|png|gitignore)$", file_name):
+                    continue
+                else:
+                    try:
+                        content = repo.get_contents(
+                            file_name, ref=commit.sha
+                        ).decoded_content
 
-                    # Sending the code to ChatGPT
-                    response = openai.Completion.create(
-                        engine=args.openai_engine,
-                        prompt=(
-                            f"Review the following {lang} code snippet, give me maximum 10 unique important suggestions to improve and optimize this code without give corrected code snippets:\n```{content}```"
-                        ),
-                        temperature=float(args.openai_temperature),
-                        max_tokens=int(args.openai_max_tokens),
-                    )
+                        # Sending the code to ChatGPT
+                        response = openai.Completion.create(
+                            engine=args.openai_engine,
+                            prompt=(
+                                f"Review the following {lang} code snippet, give me maximum 10 unique important suggestions to improve and optimize this code without give corrected code snippets:\n```{content}```"
+                            ),
+                            temperature=float(args.openai_temperature),
+                            max_tokens=int(args.openai_max_tokens),
+                        )
 
-                    # Adding a comment to the pull request with ChatGPT's response
-                    pull_request.create_issue_comment(
-                        f"I have compiled a few suggestions for this file `{file.filename}`:\n {response['choices'][0]['text']}"
-                    )
+                        # Adding a comment to the pull request with ChatGPT's response
+                        pull_request.create_issue_comment(
+                            f"I have compiled a few suggestions for this file `{file.filename}`:\n {response['choices'][0]['text']}"
+                        )
 
-                    # Add the processed file name to the set
-                    processed_files.add(file_name)
-                except Exception as e:
-                    error_message = str(e)
-                    print(error_message)
-                    # Post a comment instead of throwing an exception
-                    pull_request.create_issue_comment(
-                        f"This file `{file_name}` has exceeded the maximum token limit that ChatGPT can process, so no suggestions can be provided."
-                    )
+                        # Add the processed file name to the set
+                        processed_files.add(file_name)
+                    except Exception as e:
+                        error_message = str(e)
+                        print(error_message)
+                        # Post a comment instead of throwing an exception
+                        pull_request.create_issue_comment(
+                            f"This file `{file_name}` has exceeded the maximum token limit that ChatGPT can process, so no suggestions can be provided."
+                        )
 
 
 def patch():
